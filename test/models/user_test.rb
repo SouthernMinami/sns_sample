47796af43e16fd0561207cf1f312d95f6858582f
github MinamisoutH <kanou373@gmail.com>
2021-06-24 22:57:17 +0900

    Validations for users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: 'Example User', email: 'user@example.com')
+  end
+
+  test 'ユーザーが有効であること' do
+    assert @user.valid?
+  end
+
+  test 'ユーザー名が存在すること' do
+    @user.name = ' '
+    assert_not @user.valid?
+  end
+
+  test 'メールアドレスが存在すること' do
+    @user.email = ' '
+    assert_not @user.valid?
+  end
+
+  test 'ユーザー名が60文字以下であること' do
+    @user.name = 'a' * 61
+    assert_not @user.valid?
+  end
+
+  test 'メールアドレスが文字以下であること' do
+    @user.name = 'a' * 244 + '@example.com'
+    assert_not @user.valid?
+  end
+
+  test 'メールアドレスのフォーマットが正しいこと' do
+    valid_addresses = %w[hoge@gmail.com FUGA@foo.JP US_er@FOO.BAR.ORG a+b@gmail.COM]
+    valid_addresses.each do |valid_addresses|
+      @user.email = valid_addresses
+      assert @user.valid?, "#{valid_addresses} should be valid"
+    end
+  end
+
+  test 'メールアドレスが一意であること' do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.downcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
